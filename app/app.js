9906021b0913039098264b10da2ff3bb60d6de99
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-11-16 14:40:11 -0600

    On lecture 43, Routing. Preparing for that.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,3 @@
-// Option 1
-// var app = angular.module('customersApp', []);
-
-// Option 2 and 3
 (function() {
     var app = angular.module('customersApp', []);
 }());
--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,43 +1,3 @@
-// Option 1
-
-//app.controller('CustomersController', function ($scope) {
-//    $scope.sortBy = 'name';
-//    $scope.reverse = false;
-//    $scope.customers = [
-//        {joined: '2000-12-02', name:'Omar', city: 'San Antonio', orderTotal: '9.9956'},
-//        {joined: '2013-01-09', name:'Becky', city: 'Changsha', orderTotal: '11.0242'},
-//        {joined: '1943-06-22', name:'Mateo', city: 'Boston', orderTotal: '5.6424'},
-//        {joined: '1985-03-18', name:'Josef', city: 'Vladivostok', orderTotal: '110.9961'},
-//        {joined: '2002-07-24', name:'Diego', city: 'San Diego', orderTotal: '900.9392'}
-//    ];
-//    $scope.doSort = function(propName) {
-//        $scope.sortBy = propName;
-//        $scope.reverse = !$scope.reverse
-//    };
-//});
-
-// Option 2
-
-//(function() {
-//    angular.module('customersApp').controller('CustomersController', function ($scope) {
-//        $scope.sortBy = 'name';
-//        $scope.reverse = false;
-//        $scope.customers = [
-//            {joined: '2000-12-02', name:'Omar', city: 'San Antonio', orderTotal: '9.9956'},
-//            {joined: '2013-01-09', name:'Becky', city: 'Changsha', orderTotal: '11.0242'},
-//            {joined: '1943-06-22', name:'Mateo', city: 'Boston', orderTotal: '5.6424'},
-//            {joined: '1985-03-18', name:'Josef', city: 'Vladivostok', orderTotal: '110.9961'},
-//            {joined: '2002-07-24', name:'Diego', city: 'San Diego', orderTotal: '900.9392'}
-//        ];
-//        $scope.doSort = function(propName) {
-//            $scope.sortBy = propName;
-//            $scope.reverse = !$scope.reverse
-//        };
-//    });
-//}());
-
-// Option 3
-
 (function() {
 
     var CustomersController = function ($scope) {
@@ -55,6 +15,9 @@
             $scope.reverse = !$scope.reverse
         };
     };
+
+    // How to deal with Script Minifiers
+    CustomersController.$inject = ['$scope'];
 
     angular.module('customersApp').controller('CustomersController', CustomersController);
 
