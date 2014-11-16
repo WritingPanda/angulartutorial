5fd0fb6efb6b87c6e174a1aeea2288b7821e980f
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-11-16 13:00:17 -0600

    Preparing for learning about controllers and the ng-controller directive.

---
+++ app/controllers/customersController.js
@@ -0,0 +1,15 @@
+function CustomersController($scope) {
+    $scope.sortBy = 'name';
+    $scope.reverse = false;
+    $scope.customers = [
+        {joined: '2000-12-02', name:'Omar', city: 'San Antonio', orderTotal: '9.9956'},
+        {joined: '2013-01-09', name:'Becky', city: 'Changsha', orderTotal: '11.0242'},
+        {joined: '1943-06-22', name:'Mateo', city: 'Boston', orderTotal: '5.6424'},
+        {joined: '1985-03-18', name:'Josef', city: 'Vladivostok', orderTotal: '110.9961'},
+        {joined: '2002-07-24', name:'Diego', city: 'San Diego', orderTotal: '900.9392'}
+    ];
+    $scope.doSort = function(propName) {
+        $scope.sortBy = propName;
+        $scope.reverse = !$scope.reverse
+    };
+}
