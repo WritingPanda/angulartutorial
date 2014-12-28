cf1a29812a5be11366d58487245091391a4d28b4
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-12-27 21:35:31 -0600

    Confused again at this point. Tutorial is using an older version of Express, and I am using a newer version. How is he serving up a page using Node?

--- server.js
+++ server.js
@@ -1,23 +1,8 @@
-var express = require('express'),
-    app = express();
+var express = require('express');
+var app = express();
 
-app.use(express.static(__dirname + '/'));
-
-app.get('/customers', function(req, res) {
-    res.json(customers);
-});
-
-app.get('/orders', function(req, res) {
-    var orders = [];
-    for (var i = 0, len = customers.length; i < len; i++) {
-        if (customers[i].orders) {
-            for (var j = 0, ordersLen = customers[i].orders.length; j < ordersLen; j++) {
-                orders.push(customers[i].orders[j]);
-            }
-        }
-    }
-    res.json(orders);
-});
+// Express 4.x
+var router = express.Router();
 
 app.get('/customers/:id', function(req, res) {
     var customerId = parseInt(req.params.id);
@@ -31,20 +16,14 @@ app.get('/customers/:id', function(req, res) {
     res.json(data);
 });
 
-app.delete('/customers/:id', function(req, res) {
-    var customerId = parseInt(req.params.id);
-    var data = { status: true };
-    for (var i = 0, len = customers.length; i < len; i++) {
-        if (customers[i].id === customerId) {
-            customers.splice(i, 1);
-            data = { status: true };
-            break;
-        }
-    }
-    res.json(data);
+app.get('/customers', function(req, res) {
+    res.json(customers);
+    res.send("hello");
 });
 
 app.listen(8080);
+
+console.log('Express listening on port 8080');
 
 // No DB. Customer data is here.
 var customers = [
@@ -125,20 +104,6 @@ var customers = [
                 id: 7,
                 product: 'Silver leash',
                 total: 300.8952
-            }
-        ]
-    },
-    {
-        id: 6,
-        joined: '2014-12-25',
-        name: 'Santa Claus',
-        city: 'North Pole',
-        orderTotal: 500.00,
-        orders: [
-            {
-                id: 8,
-                product: 'Sleigh',
-                total: 500.00
             }
         ]
     }
