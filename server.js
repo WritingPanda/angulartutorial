9d839266b90e80c334a8b720e0b94622679af62f
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-12-27 21:48:38 -0600

    Git error for some reason. Fixed it and updated code. Checked if everything worked. It does. For now... eep.

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
 
-// Express 4.x
-var router = express.Router();
+app.use(express.static(__dirname + '/'));
+
+app.get('/customers', function(req, res) {
+    res.json(customers);
+});
+
+app.get('/orders', function(req, res){
+    var orders = [];
+    for (var i = 0, len = customers.length; i < len; i++) {
+        if (customers[i].orders) {
+            for (var j = 0, ordersLen = customers[i].orders.length; j < ordersLen; j++) {
+                orders.push(customers[i].orders[j]);
+            }
+        }
+    }
+    res.json(orders);
+});
 
 app.get('/customers/:id', function(req, res) {
     var customerId = parseInt(req.params.id);
@@ -16,13 +31,20 @@ app.get('/customers/:id', function(req, res) {
     res.json(data);
 });
 
-app.get('/customers', function(req, res) {
-    res.json(customers);
-    res.send("hello");
+app.delete('/customers/:id', function(req, res) {
+    var customerId = parseInt(req.params.id);
+    var data = { status: true };
+    for (var i = 0, len = customers.length; i < len; i++) {
+        if (customers[i].id === customerId) {
+            customers.splice(i, 1);
+            data = { status: true };
+            break;
+        }
+    }
+    res.json(data);
 });
 
 app.listen(8080);
-
 console.log('Express listening on port 8080');
 
 // No DB. Customer data is here.
@@ -104,6 +126,20 @@ var customers = [
                 id: 7,
                 product: 'Silver leash',
                 total: 300.8952
+            }
+        ]
+    },
+    {
+        id: 6,
+        joined: '2014-12-25',
+        name: 'Santa Claus',
+        city: 'North Pole',
+        orderTotal: 500.00,
+        orders: [
+            {
+                id: 8,
+                product: 'Sleigh',
+                total: 500.00
             }
         ]
     }
